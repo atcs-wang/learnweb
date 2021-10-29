d9dc7b2204e68aaa0a5f805ee6910622b663d489
Matthew Wang <matwan@bergen.org>
2021-10-29 15:09:55 -0400

    added intro-to-js materials, also later todo-list versions

---
+++ intro-to-js/ex1.js
@@ -0,0 +1,10 @@
+// A basic example
+
+alert('Hello World!');
+//Greet the user
+let message = "Hello User!";
+alert(message);
+let name = prompt("Actually, what is your name?");
+let newMessage = "Greetings, " + name;
+alert(newMessage);
+
